bb3eb37e3ce3766eaebe79d8de117a7e01d784d1
buzruk <buzurgmexrs@gmail.com>
2024-01-27 15:50:15 +0500

    prepare for Heroku

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-x2nj27!kh*43mk2qrbwp_b=7=m$$t1xh9^tut8)6$3w+lmo(y2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
